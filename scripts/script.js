dbdcb34d621dd3c358bf7ae91ccd92cb94bb836f
elisepinot <elise.pinot@live.fr>
2023-08-29 14:42:04 +0200

    Adding main-research-algo2.js file + edition of script.js with import of the research function

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 /*********** Data import ***********/
 import { recipesCards } from "./recipe-card-factory.js";
 import { totalNumberOfRecipeCards } from "./recipes-counter.js";
+import { searchRecipe2 } from "./main-research-algo2.js";
 import { numberOfRecipeCards } from "./recipes-counter.js";
 import { deleteInput } from "./delete-user-input.js";
 
@@ -9,7 +10,7 @@ export const searchInput = document.querySelector(".main-search__input");
 export const recipeCards = document.querySelectorAll(".recipe-card");
 
 /********** Research ***********/
-// searchInput.addEventListener("input", searchRecipe1);
+searchInput.addEventListener("input", searchRecipe2);
 
 /********** Recipes Cards counter **********/
 totalNumberOfRecipeCards();
